249e86595d2bf0f69aea52cc3b3e0fbbff8d6c01
anmol thapar <mr.anmolthapar@gmail.com>
2025-05-23 10:07:21 +0100

    refactor: remove individual Electron app build steps from GitHub Actions workflow and ensure build.sh handles the process

--- build.sh
+++ build.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-
 set -eu
 
-HERE=$(dirname "$0")
+HERE=$(cd "$(dirname "$0")" && pwd)
 PLATFORM="$(uname | tr '[:upper:]' '[:lower:]')"
 
 # Build Rust file service
-$HERE/file-service/build.sh
+"$HERE/file-service/build.sh"
 
 # Copy Rust binary to frontend/resources/$PLATFORM
-mkdir -p $HERE/frontend/resources/$PLATFORM
-cp -f $HERE/file-service/target/release/file-service $HERE/frontend/resources/$PLATFORM/file-service
+mkdir -p "$HERE/frontend/resources/$PLATFORM"
+cp -f "$HERE/file-service/target/release/file-service" "$HERE/frontend/resources/$PLATFORM/file-service"
 
 # Build Kotlin backend
-$HERE/backend/build.sh
+"$HERE/backend/build.sh"
 # Copy fat.jar to frontend/resources/$PLATFORM
-cp -f $HERE/backend/app/build/libs/fat.jar $HERE/frontend/resources/$PLATFORM/fat.jar
+cp -f "$HERE/backend/app/build/libs/fat.jar" "$HERE/frontend/resources/$PLATFORM/fat.jar"
 
-# Build frontend
-$HERE/frontend/build.sh
-
+# Build electron app
+cd "$HERE/frontend"
+npm install
+npm run electron:build
+cd "$HERE"
 
